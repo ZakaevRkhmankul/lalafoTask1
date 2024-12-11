79f4a6a2c537fc4af98a6d5298f5255e39bde441
user <vaamfaf@gmail.com>
2024-12-11 16:04:54 +0600

    writed User CRUD methods

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -1,6 +1,14 @@
 package dao;
 
-public interface UserDao {
+import models.User;
 
+import java.util.List;
+
+public interface UserDao {
+    void addUser(User user);
+    List<User> getUsers();
+    User getUser(Long id);
+    void updateUserById(Long id,String username,String password,String newPassword);
+    void deleteUserById(Long id);
 
 }
